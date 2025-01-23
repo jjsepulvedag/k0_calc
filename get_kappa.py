8f076f9c3c99e4129b6a93ade09cdd17aeaa4bfb
JJSG - UC <juanjose.sepulvedagarcia@pg.canterbury.ac.nz>
2025-01-24 12:22:27 +1300

    Changes to get_kappa module

---
+++ get_kappa.py
@@ -0,0 +1,17 @@
+import numpy as np 
+
+def kappa_1rcrd(freqs, FAmps, f1, f2):
+
+    return None
+
+def kappa_VH14():
+
+    return None 
+
+def kappa_RMS():
+    
+    return None
+
+def kappa_AVG():
+    
+    return None
