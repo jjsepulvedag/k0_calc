ef311cc0b35f19b64bda08b33dd7631241111221
JJSG - UC <juanjose.sepulvedagarcia@pg.canterbury.ac.nz>
2025-06-13 18:05:03 +1200

    I updated all the function in get_kappa.py. All of them return a python dataframe now, with the value of kappa, intercept, and their standard errors.

--- get_kappa.py
+++ get_kappa.py
@@ -1,4 +1,5 @@
 import numpy as np 
+import pandas as pd
 import scipy.stats as st
 from k0_calc.miscK import get_FAS
 from k0_calc.miscK import rotateGM
@@ -6,23 +7,23 @@ from k0_calc.miscK import rotateGM
 
 def kappa_1GMR(acc, dt, f1, f2):
     # Create a DF for storing coefficients and stderrs
-    dfCols = ['Slope', 'Intercept']
+    dfCols = ['Kappa', 'Intercept']
     dfRows = ['Mean', 'Stderr']
-    kappaDF = pd.dataframe(None, index=dfRows, columns=dfCols)
-
+    linRegDF = pd.DataFrame(None, index=dfRows, columns=dfCols)
+    
     # Conduct the linear regression
     fas, ff = get_FAS(acc, dt)
     idx = np.where((f1<=ff) & (ff<=f2))
     results = st.linregress(ff[idx], np.log(fas[idx]))
-
+    
     # Storing results in the DataFrame
-    # Remember that the direct slope is lambda, which is lambda=-kappa*pi
-    kappaDF['Mean', 'Slope'] = - results.slope/np.pi
-    kappaDF['Stderr', 'Slope'] = results.stderr/np.pi
-    kappaDF['Mean', 'Intercept'] = results.intercept
-    kappaDF['Stderr', 'Intercept'] = results.intercept_stderr
+    # Remember that the direct slope lambda=-kappa*pi
+    linRegDF.loc['Mean', 'Kappa'] = - results.slope/np.pi
+    linRegDF.loc['Stderr', 'Kappa'] = results.stderr/np.pi
+    linRegDF.loc['Mean', 'Intercept'] = results.intercept
+    linRegDF.loc['Stderr', 'Intercept'] = results.intercept_stderr
 
-    return kappaDF
+    return linRegDF
 
 def kappa_1FAS(fas, ff, f1, f2):
     '''
@@ -39,21 +40,21 @@ def kappa_1FAS(fas, ff, f1, f2):
     '''
 
     # Create a DF for storing coefficients and stderrs
-    dfCols = ['Slope', 'Intercept']
+    dfCols = ['Kappa', 'Intercept']
     dfRows = ['Mean', 'Stderr']
-    kappaDF = pd.dataframe(None, index=dfRows, columns=dfCols)
+    linRegDF = pd.DataFrame(None, index=dfRows, columns=dfCols)
 
     idx = np.where((f1<=ff) & (ff<=f2))
     results = st.linregress(ff[idx], np.log(fas[idx]))
 
     # Storing results in the DataFrame
-    # Remember that the direct slope is lambda, which is lambda=-kappa*pi
-    kappaDF['Mean', 'Slope'] = - results.slope/np.pi
-    kappaDF['Stderr', 'Slope'] = results.stderr/np.pi
-    kappaDF['Mean', 'Intercept'] = results.intercept
-    kappaDF['Stderr', 'Intercept'] = results.intercept_stderr
+    # Remember that the direct slope lambda=-kappa*pi
+    linRegDF.loc['Mean', 'Kappa'] = - results.slope/np.pi
+    linRegDF.loc['Stderr', 'Kappa'] = results.stderr/np.pi
+    linRegDF.loc['Mean', 'Intercept'] = results.intercept
+    linRegDF.loc['Stderr', 'Intercept'] = results.intercept_stderr
 
-    return kappaDF
+    return linRegDF
 
 def kappa_VH14(acc1, acc2, dt, N, f1, f2):
     '''
@@ -75,17 +76,24 @@ def kappa_VH14(acc1, acc2, dt, N, f1, f2):
         - For a dStep of 30°, use N=07
         - For a dStep of 45°, use N=05
     '''
+    dfCols = ['Kappa', 'Intercept']
+    dfRows = ['Mean', 'Stderr']
+    avgLinRegDF = pd.DataFrame(None, index=dfRows, columns=dfCols)
     thetas = np.radians(np.linspace(0, 180, N))
-    kappas = np.zeros((thetas.shape[0], 2))
+    allLinRegDF = np.zeros((thetas.shape[0], 2, 2)) # N rotations, 2r, 2c
 
     for i in range(thetas.shape[0]):
         acc_r = rotateGM(acc1, acc2, thetas[i]) # get rotated ground motion
-        kappas[i, :], xxx = kappa_1GMR(acc_r, dt, f1, f2)
+        allLinRegDF[i] = kappa_1GMR(acc_r, dt, f1, f2).values
     
     # Van Houtte et al. (2014) uses mean. Rotd50 is computed with median
-    kappaVH14 = np.mean(kappas, axis=0) 
+    avgLinRegDF.loc['Mean', 'Kappa'] = allLinRegDF[:, 0, 0].mean()
+    avgLinRegDF.loc['Mean', 'Intercept'] = allLinRegDF[:, 0, 1].mean()
+    avgLinRegDF.loc['Stderr', 'Kappa'] = np.sqrt(1 / (1/(allLinRegDF[:, 1, 0]**2)).sum())
+    avgLinRegDF.loc['Stderr', 'Intercept'] = np.sqrt(1 / (1/(allLinRegDF[:, 1, 1]**2)).sum())
 
-    return kappaVH14
+    return avgLinRegDF
+
 
 def kappa_RMS(acc1, acc2, dt, f1, f2):
 
@@ -94,29 +102,28 @@ def kappa_RMS(acc1, acc2, dt, f1, f2):
 
     H_f = np.sqrt(0.5*(fas000**2 + fas090**2))
 
-    kappaRMS, interceptRMS = kappa_1FAS(H_f, ff000, f1, f2)
+    linRegDF = kappa_1FAS(H_f, ff000, f1, f2)
     
-    return kappaRMS
+    return linRegDF
 
-
-def kappa_GM(acc1, acc2, dt, f1, f2):
-    '''
-    Not recommended. It crashes when kappa is negative in one direction. 
-    '''
-    
-    kappa000, intercept_000 = kappa_1GMR(acc1, dt, f1, f2)
-    kappa090, intercept_090 = kappa_1GMR(acc2, dt, f1, f2)
-
-    kappaGM = np.sqrt(kappa000*kappa090)
-
-    return kappaGM
 
 def kappa_Mean(acc1, acc2, dt, f1, f2):
-    kappa000, intercept000 = kappa_1GMR(acc1, dt, f1, f2)
-    kappa090, intercept090 = kappa_1GMR(acc2, dt, f1, f2)
-    kappaAVG = np.mean([[kappa000, kappa090]])
 
-    return kappaAVG
+    dfCols = ['Kappa', 'Intercept']
+    dfRows = ['Mean', 'Stderr']
+    avgLinRegDF = pd.DataFrame(None, index=dfRows, columns=dfCols)
+    dimens = 2 # We compute two values of kappa and intercept
+    allLinRegDF = np.zeros((dimens, 2, 2)) # 2d, 2r, 2c
+
+    allLinRegDF[0] = kappa_1GMR(acc1, dt, f1, f2).values
+    allLinRegDF[1] = kappa_1GMR(acc2, dt, f1, f2).values
+
+    avgLinRegDF.loc['Mean', 'Kappa'] = allLinRegDF[:, 0, 0].mean()
+    avgLinRegDF.loc['Mean', 'Intercept'] = allLinRegDF[:, 0, 1].mean()
+    avgLinRegDF.loc['Stderr', 'Kappa'] = np.sqrt(1 / (1/(allLinRegDF[:, 1, 0]**2)).sum())
+    avgLinRegDF.loc['Stderr', 'Intercept'] = np.sqrt(1 / (1/(allLinRegDF[:, 1, 1]**2)).sum())
+
+    return avgLinRegDF
 
 if __name__=='__main__':
 
